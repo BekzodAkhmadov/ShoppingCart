d717d4115458822f1613385e1abea811b3ed6e10
Bekzod <ahmadovbekzod94@gmail.com>
2025-01-23 23:18:05 +0500

    first commit

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+namespace Test;
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var actualValue = 2;
+        Assert.Equal(2, actualValue);
+    }
+    [Fact]
+    public void DoTheThing()
+    {
+        var x = 1;
+        Assert.Equal(1, x);
+    }
+}
